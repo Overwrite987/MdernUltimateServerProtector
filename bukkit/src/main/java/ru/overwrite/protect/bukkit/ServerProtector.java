ae4a677d812c83d6dc62972c72df2d795d3fa831
Overwrite987 <artem.korolev_2003@mail.ru>
2025-03-10 19:03:19 +0300

    YEAH BITCH! HERE WE GO!!!

--- bukkit/src/main/java/ru/overwrite/protect/bukkit/ServerProtector.java
+++ bukkit/src/main/java/ru/overwrite/protect/bukkit/ServerProtector.java
@@ -6,6 +6,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.messaging.Messenger;
+import ru.overwrite.protect.bukkit.utils.logging.Logger;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +15,9 @@ public final class ServerProtector extends ServerProtectorManager {
     @Override
     public void onEnable() {
         long startTime = System.currentTimeMillis();
+        if (!isPaper()) {
+            return;
+        }
         saveDefaultConfig();
         final FileConfiguration config = getConfig();
         final ConfigurationSection mainSettings = config.getConfigurationSection("main-settings");
@@ -33,6 +37,22 @@ public final class ServerProtector extends ServerProtectorManager {
         checkForUpdates(mainSettings);
         long endTime = System.currentTimeMillis();
         getPluginLogger().info("Plugin started in " + (endTime - startTime) + " ms");
+    }
+
+    public boolean isPaper() {
+        if (getServer().getName().equals("CraftBukkit")) {
+            Logger pluginLogger = getPluginLogger();
+            pluginLogger.info(" ");
+            pluginLogger.info("§6============= §c! WARNING ! §6=============");
+            pluginLogger.info("§eЭтот плагин работает только на Paper и его форках!");
+            pluginLogger.info("§eАвтор плагина §cкатегорически §eвыступает за отказ от использования устаревшего и уязвимого софта!");
+            pluginLogger.info("§eСкачать Paper: §ahttps://papermc.io/downloads/all");
+            pluginLogger.info("§6============= §c! WARNING ! §6=============");
+            pluginLogger.info(" ");
+            this.setEnabled(false);
+            return false;
+        }
+        return true;
     }
 
     @Override
